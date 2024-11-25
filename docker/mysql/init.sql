d14adeb94fdb988e17e960bfb725a3bbce0b86b4
DongWoonKim <01030985843z@naver.com>
2024-11-25 16:46:57 +0900

    update code

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -1,9 +1,10 @@
 CREATE DATABASE IF NOT EXISTS catalog;
 CREATE DATABASE IF NOT EXISTS `order`;
+CREATE DATABASE IF NOT EXISTS auth;
 
 -- 필요한 경우, 데이터베이스에 대한 추가 설정도 가능합니다.
 -- 예: 사용자 권한 설정
-GRANT ALL PRIVILEGES ON catalog.* TO 'catalog_user'@'%';
-GRANT ALL PRIVILEGES ON `order`.* TO 'catalog_user'@'%';
+GRANT ALL PRIVILEGES ON catalog.* TO 'user'@'%';
+GRANT ALL PRIVILEGES ON `order`.* TO 'user'@'%';
 
 FLUSH PRIVILEGES;
